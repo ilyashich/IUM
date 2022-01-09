07c8f880574fad02a8199ecf0f94f4ca921a1802
Illia Yatskevich <i.yatskevich@stud.elka.pw.edu.pl>
2022-01-09 12:33:43 +0100

    mlp class

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -1,23 +1,19 @@
-import sklearn as sk
 import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
 
 
-def read_data():
-    data = pd.read_csv('../data/data_with_categories.csv')
-    col_to_predict = 'successful'
+class MLP:
+    def __init__(self, X, y):
+        self.__mlp = MLPClassifier(hidden_layer_sizes=(150, 100, 50), max_iter=300, activation='relu', solver='adam',
+                                   random_state=1)
+        self.__mlp.fit(X, y)
 
-    y = data[col_to_predict]
-    X = data.drop([col_to_predict], axis=1)
-    return X, y
+    def mlp_get_score(self, X, y):
+        return self.__mlp.score(X, y)
 
-
-def accuracy(conf_matrix):
-    diagonal_sum = conf_matrix.trace()
-    sum_of_all_elements = conf_matrix.sum()
-    return diagonal_sum / sum_of_all_elements
+    def mlp_predict(self, X):
+        return self.__mlp.predict(X)
 
 
 if __name__ == '__main__':
@@ -29,14 +25,7 @@ if __name__ == '__main__':
     X_test = validation_set.iloc[:, validation_set.columns != 'successful'].values
     y_test = validation_set.iloc[:, 3].values
 
-    classifier = MLPClassifier(hidden_layer_sizes=(150, 100, 50), max_iter=300, activation='relu', solver='adam',
-                               random_state=1)
-    classifier.fit(X_train, y_train)
-    y_pred = classifier.predict(X_test)
+    mlp = MLP(X_train, y_train)
+    y_pred = mlp.mlp_predict(X_test)
 
-    cm = confusion_matrix(y_pred, y_test)
-    print("Accuracy of MLPClassifier : ", accuracy(cm))
-    # NN = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-    # NN.fit(X, y)
-    # NN.predict(X.iloc[460:, :])
-    # print(round(NN.score(X, y), 4))
+    print("Accuracy of MLPClassifier : ", round(mlp.mlp_get_score(X_test, y_test), 4))
